a775751e4cb5452c1bd9c3a200660b34071f9528
candr006 <candr006@ucr.edu>
2018-12-01 05:42:19 +0000

    output k neighbors not sorted

--- KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
+++ KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import java.io.IOException;
+import java.util.Comparator;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
@@ -17,6 +19,14 @@ import scala.Tuple2;
  * KNN
  *
  */
+class TupleComparator implements Comparator<Tuple2<Integer, Integer>>, Serializable {
+    @Override
+    public int compare(Tuple2<Integer, Integer> o1, Tuple2<Integer, Integer> o2) {
+        if (o1._1() == o2._1())
+            return o1._2() - o2._2();
+        return o1._1() - o2._1();
+    }
+}
 public class KNN
 {
 
@@ -28,7 +38,7 @@ public class KNN
             System.out.println("Exiting");
             return;
         }
-        String str_local_file=args[1];
+        String str_local_file=args[0];
 
         //check if the local file exists
         File localFile= new File(str_local_file);
@@ -54,18 +64,18 @@ public class KNN
         String formatted = dtf.format(now);
         String out_path="KNN_output_"+formatted+".txt";
 
-        //q=args[2] and k=args[3]
+        //q=args[1] and k=args[2]
 
         JavaSparkContext spark =
                 new JavaSparkContext("local", "CS226-Demo");
         JavaRDD<String> logFile = spark.textFile("local_copy.csv");
 
-
+        //calculate the distance from q to k and add to a map
         JavaPairRDD<Double, String> distanceMap = logFile.mapToPair(new PairFunction<String, Double, String>() {
 
             public Tuple2<Double, String> call(String line) throws Exception {
                 String[] parts = line.split(",");
-                String[] q=args[2].split(",");
+                String[] q=args[1].split(",");
                 Double x1= Double.parseDouble(q[0]);
                 Double y1= Double.parseDouble(q[1]);
 
@@ -82,12 +92,153 @@ public class KNN
             }
         });
 
+        //now sort by distance
+        JavaPairRDD<Double, String> sortedDistance = distanceMap.sortByKey();
 
-        /*JavaPairRDD<String, Long> sumBytesByCode = codeSize.reduceByKey(new Function2<Long, Long, Long>() {
+        //remove duplicates
+        JavaPairRDD<Double, String> distinctNeighbors = sortedDistance.distinct();
+
+        //print k neighbors
+        Integer k= Integer.valueOf(args[2]);
+
+        for(Tuple2<Double, String> line:distinctNeighbors.collect()){
+            if(k>0) {
+                System.out.println("* " + line);
+                k--;
+            }else{
+                break;
+            }
+        }
+
+        /*
+        Map<String, Long> sumBytesByCodeMap = sumBytesByCode.collectAsMap();
+        System.out.println(sumBytesByCodeMap);
+        Long sumBytes = bytes.reduce(new Function2<Long, Long, Long>() {
             public Long call(Long s1, Long s2) throws Exception {
                 return s1 + s2;
             }
         });
+
+        System.out.printf("Sum Bytes is %d\n", sumBytes);
+
+        long numOfLines = okLines.count();
+        System.out.printf("%d lines with OK in the file\n", numOfLines);
+        System.out.println( "Hello World!" );*/
+    }
+}
+package edu.ucr.cs.cs226.candr006;
+        import java.io.*;
+        import java.time.LocalDateTime;
+        import java.time.format.DateTimeFormatter;
+
+        import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+        import java.io.IOException;
+        import java.util.Comparator;
+
+        import org.apache.spark.api.java.JavaPairRDD;
+        import org.apache.spark.api.java.JavaRDD;
+        import org.apache.spark.api.java.JavaSparkContext;
+        import org.apache.spark.api.java.function.Function2;
+        import org.apache.spark.api.java.function.PairFunction;
+        import scala.Tuple2;
+
+
+/**
+ * KNN
+ *
+ */
+class TupleComparator implements Comparator<Tuple2<Integer, Integer>>, Serializable {
+    @Override
+    public int compare(Tuple2<Integer, Integer> o1, Tuple2<Integer, Integer> o2) {
+        if (o1._1() == o2._1())
+            return o1._2() - o2._2();
+        return o1._1() - o2._1();
+    }
+}
+public class KNN
+{
+
+    public static void main( String[] args ) throws IOException, ClassNotFoundException, InterruptedException {
+        //check that all arguments are there
+        if(args.length<3){
+            System.out.println("\n\nERROR: You are missing one or more arguments.");
+            System.out.println("<local file path> <point q> <k>");
+            System.out.println("Exiting");
+            return;
+        }
+        String str_local_file=args[0];
+
+        //check if the local file exists
+        File localFile= new File(str_local_file);
+        if(!localFile.exists()){
+            System.out.println("\n\nERROR: The local file you entered does not exist. Exiting.\n");
+            return;
+        }
+
+        //first decompress bzip file
+        FileInputStream is4 = new FileInputStream(localFile);
+        BZip2CompressorInputStream inputStream4 = new BZip2CompressorInputStream(is4, true);
+        OutputStream ostream4 = new FileOutputStream("local_copy.csv");
+        final byte[] buffer4 = new byte[8192];
+        int n4 = 0;
+        while ((n4 = inputStream4.read(buffer4))>0) {
+            ostream4.write(buffer4, 0, n4);
+        }
+        ostream4.close();
+        inputStream4.close();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMddyyyyHHmmss");
+        LocalDateTime now = LocalDateTime.now();
+        String formatted = dtf.format(now);
+        String out_path="KNN_output_"+formatted+".txt";
+
+        //q=args[1] and k=args[2]
+
+        JavaSparkContext spark =
+                new JavaSparkContext("local", "CS226-Demo");
+        JavaRDD<String> logFile = spark.textFile("local_copy.csv");
+
+        //calculate the distance from q to k and add to a map
+        JavaPairRDD<Double, String> distanceMap = logFile.mapToPair(new PairFunction<String, Double, String>() {
+
+            public Tuple2<Double, String> call(String line) throws Exception {
+                String[] parts = line.split(",");
+                String[] q=args[1].split(",");
+                Double x1= Double.parseDouble(q[0]);
+                Double y1= Double.parseDouble(q[1]);
+
+
+                Double x2= Double.valueOf(parts[1]);
+                Double y2= Double.valueOf(parts[2]);
+
+                double dist=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+
+                String xy_string= parts[1]+","+parts[2];
+
+
+                return new Tuple2<Double,String>(dist, xy_string);
+            }
+        });
+
+        //now sort by distance
+        JavaPairRDD<Double, String> sortedDistance = distanceMap.sortByKey();
+
+        //remove duplicates
+        JavaPairRDD<Double, String> distinctNeighbors = sortedDistance.distinct();
+
+        //print k neighbors
+        Integer k= Integer.valueOf(args[2]);
+
+        for(Tuple2<Double, String> line:distinctNeighbors.collect()){
+            if(k>0) {
+                System.out.println("* " + line);
+                k--;
+            }else{
+                break;
+            }
+        }
+
+        /*
         Map<String, Long> sumBytesByCodeMap = sumBytesByCode.collectAsMap();
         System.out.println(sumBytesByCodeMap);
         Long sumBytes = bytes.reduce(new Function2<Long, Long, Long>() {
