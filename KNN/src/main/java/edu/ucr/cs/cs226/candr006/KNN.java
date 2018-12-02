b0a4426d61af388ac09faee76c511bb7ca9878ef
candr006 <candr006@ucr.edu>
2018-12-02 19:20:22 +0000

    moved code back to main. mapToPair not serializable

--- KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
+++ KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
@@ -22,67 +22,27 @@ package edu.ucr.cs.cs226.candr006;
  */
 class SparkRDDKNN
 {
-    private int k;
+
+}
+
+public class KNN
+{   private int k;
     private String q_comma;
     private String file_name;
 
-    SparkRDDKNN(int k1, String q1, String file_name1){
+    KNN(int k1, String q1, String file_name1){
         //constructor
         k=k1;
         q_comma=q1;
         file_name=file_name1;
     }
 
-    void getKNN(){
-
-        JavaSparkContext spark =
-                new JavaSparkContext("local", "SparkRDD-KNN");
-        JavaRDD<String> logFile = spark.textFile("local_copy.csv");
-
-        //calculate the distance from q to k and add to a map
-        JavaPairRDD<Double, String> distanceMap = logFile.mapToPair(new PairFunction<String, Double, String>() {
-
-            public Tuple2<Double, String> call(String line) throws Exception {
-                String[] parts = line.split(",");
-                String[] q=q_comma.split(",");
-                Double x1= Double.parseDouble(q[0]);
-                Double y1= Double.parseDouble(q[1]);
+    void getSparkRDDKNN(){
 
 
-                Double x2= Double.valueOf(parts[1]);
-                Double y2= Double.valueOf(parts[2]);
-
-                double dist=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-
-                String xy_string= parts[1]+","+parts[2];
-
-
-                return new Tuple2<Double,String>(dist, xy_string);
-            }
-        });
-
-        //now sort by distance
-        JavaPairRDD<Double, String> sortedDistance = distanceMap.sortByKey(true);
-
-        //remove duplicates
-        JavaPairRDD<Double, String> distinctNeighbors = sortedDistance.distinct();
-
-        //print k neighbors
-        //Integer k= Integer.valueOf(args[2]);
-
-        for(Tuple2<Double, String> line:distinctNeighbors.sortByKey().collect()){
-            if(k>0) {
-                System.out.println("* " + line);
-                k--;
-            }else{
-                break;
-            }
-        }
     }
-}
 
-public class KNN
-{
+
     public static void decompressFile(File localFile) throws IOException {
         //first decompress bzip file
         System.out.println("\nDecompressing File. Please wait a few moments...\n");
@@ -101,6 +61,7 @@ public class KNN
         LocalDateTime now = LocalDateTime.now();
         String formatted = dtf.format(now);
         String out_path="KNN_output_"+formatted+".txt";
+        System.out.println("test");
         return;
     }
 
@@ -123,7 +84,7 @@ public class KNN
 
 
         //q=args[1] and k=args[2]
-        SparkRDDKNN alg1 = new SparkRDDKNN(Integer.valueOf(args[2]), args[1], "local_copy.csv");
+        KNN knn_ = new KNN(Integer.valueOf(args[2]), args[1], "local_copy.csv");
 
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         int n=-1;
@@ -136,7 +97,55 @@ public class KNN
                 if(first_run){
                     decompressFile(localFile);
                 }
-                alg1.getKNN();
+
+                //knn_.getSparkRDDKNN();
+                JavaSparkContext spark =
+                        new JavaSparkContext("local", "SparkRDD-KNN");
+                JavaRDD<String> logFile = spark.textFile("local_copy.csv");
+
+                //calculate the distance from q to k and add to a map
+                JavaPairRDD<Double, String> distanceMap = logFile.mapToPair(new PairFunction<String, Double, String>() {
+
+                    public Tuple2<Double, String> call(String line) throws Exception {
+                        String[] parts = line.split(",");
+                        String[] q=knn_.q_comma.split(",");
+                        Double x1= Double.parseDouble(q[0]);
+                        Double y1= Double.parseDouble(q[1]);
+
+
+                        Double x2= Double.valueOf(parts[1]);
+                        Double y2= Double.valueOf(parts[2]);
+
+                        double dist=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+
+                        String xy_string= parts[1]+","+parts[2];
+
+
+                        return new Tuple2<Double,String>(dist, xy_string);
+                    }
+                });
+
+                //now sort by distance
+                JavaPairRDD<Double, String> sortedDistance = distanceMap.sortByKey(true);
+
+                //remove duplicates
+                JavaPairRDD<Double, String> distinctNeighbors = sortedDistance.distinct();
+
+                //print k neighbors
+                //Integer k= Integer.valueOf(args[2]);
+
+                for(Tuple2<Double, String> line:distinctNeighbors.sortByKey().collect()){
+                    if(knn_.k>0) {
+                        System.out.println("* " + line);
+                        knn_.k--;
+                    }else{
+                        break;
+                    }
+                }
+
+
+
+
             }else if(n==2){
                 if(first_run){
                     decompressFile(localFile);
