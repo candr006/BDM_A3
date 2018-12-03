46251de390afe81c07423fab271da3c54e2a3cb2
candr006 <candr006@ucr.edu>
2018-12-03 02:36:55 +0000

    spark rdd to output file

--- KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
+++ KNN/src/main/java/edu/ucr/cs/cs226/candr006/KNN.java
@@ -1,69 +1,30 @@
 package edu.ucr.cs.cs226.candr006;
-        import java.io.*;
-        import java.time.LocalDateTime;
-        import java.time.format.DateTimeFormatter;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-        import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
-        import java.io.IOException;
-        import java.util.Comparator;
-        import java.util.Scanner;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import java.io.IOException;
+import java.util.Comparator;
 
-        import org.apache.spark.api.java.JavaPairRDD;
-        import org.apache.spark.api.java.JavaRDD;
-        import org.apache.spark.api.java.JavaSparkContext;
-        import org.apache.spark.api.java.function.Function2;
-        import org.apache.spark.api.java.function.PairFunction;
-        import scala.Tuple2;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
 
 /**
  * KNN
  *
  */
-class SparkRDDKNN
-{
-
-}
 
 public class KNN
-{   private int k;
-    private String q_comma;
-    private String file_name;
-
-    KNN(int k1, String q1, String file_name1){
-        //constructor
-        k=k1;
-        q_comma=q1;
-        file_name=file_name1;
-    }
-
-    void getSparkRDDKNN(){
-
-
-    }
-
-
-    public static void decompressFile(File localFile) throws IOException {
-        //first decompress bzip file
-        System.out.println("\nDecompressing File. Please wait a few moments...\n");
-        FileInputStream is4 = new FileInputStream(localFile);
-        BZip2CompressorInputStream inputStream4 = new BZip2CompressorInputStream(is4, true);
-        OutputStream ostream4 = new FileOutputStream("local_copy.csv");
-        final byte[] buffer4 = new byte[8192];
-        int n4 = 0;
-        while ((n4 = inputStream4.read(buffer4))>0) {
-            ostream4.write(buffer4, 0, n4);
-        }
-        ostream4.close();
-        inputStream4.close();
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMddyyyyHHmmss");
-        LocalDateTime now = LocalDateTime.now();
-        String formatted = dtf.format(now);
-        String out_path="KNN_output_"+formatted+".txt";
-        System.out.println("test");
-        return;
-    }
+{
 
     public static void main( String[] args ) throws IOException, ClassNotFoundException, InterruptedException {
         //check that all arguments are there
@@ -82,83 +43,92 @@ public class KNN
             return;
         }
 
+        //first decompress bzip file
+        FileInputStream is4 = new FileInputStream(localFile);
+        BZip2CompressorInputStream inputStream4 = new BZip2CompressorInputStream(is4, true);
+        OutputStream ostream4 = new FileOutputStream("local_copy.csv");
+        final byte[] buffer4 = new byte[8192];
+        int n4 = 0;
+        while ((n4 = inputStream4.read(buffer4))>0) {
+            ostream4.write(buffer4, 0, n4);
+        }
+        ostream4.close();
+        inputStream4.close();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMddyyyyHHmmss");
+        LocalDateTime now = LocalDateTime.now();
+        String formatted = dtf.format(now);
+        String out_path_rdd="SparkRDD_KNN_output_"+formatted+".txt";
+        String out_path_sql="SparkSQL_KNN_output_"+formatted+".txt";
 
         //q=args[1] and k=args[2]
-        KNN knn_ = new KNN(Integer.valueOf(args[2]), args[1], "local_copy.csv");
+        /**********************SparkRDD Implementation**********************
+         *
+         */
 
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        int n=-1;
-        Boolean first_run=true;
-        System.out.println("\nSelect one:\n1. Spark RDD KNN \n2. Spark SQL KNN \n3. Exit \n \n");
+        JavaSparkContext spark =
+                new JavaSparkContext("local", "CS226-Demo");
+        JavaRDD<String> logFile = spark.textFile("local_copy.csv");
 
-        while (n!=3) {
-            n = reader.nextInt();
-            if(n==1){
-                if(first_run){
-                    decompressFile(localFile);
-                }
+        //calculate the distance from q to k and add to a map
+        JavaPairRDD<Double, String> distanceMap = logFile.mapToPair(new PairFunction<String, Double, String>() {
 
-                //knn_.getSparkRDDKNN();
-                JavaSparkContext spark =
-                        new JavaSparkContext("local", "SparkRDD-KNN");
-                JavaRDD<String> logFile = spark.textFile("local_copy.csv");
-
-                //calculate the distance from q to k and add to a map
-                JavaPairRDD<Double, String> distanceMap = logFile.mapToPair(new PairFunction<String, Double, String>() {
-
-                    public Tuple2<Double, String> call(String line) throws Exception {
-                        String[] parts = line.split(",");
-                        String[] q=knn_.q_comma.split(",");
-                        Double x1= Double.parseDouble(q[0]);
-                        Double y1= Double.parseDouble(q[1]);
+            public Tuple2<Double, String> call(String line) throws Exception {
+                String[] parts = line.split(",");
+                String[] q=args[1].split(",");
+                Double x1= Double.parseDouble(q[0]);
+                Double y1= Double.parseDouble(q[1]);
 
 
-                        Double x2= Double.valueOf(parts[1]);
-                        Double y2= Double.valueOf(parts[2]);
+                Double x2= Double.valueOf(parts[1]);
+                Double y2= Double.valueOf(parts[2]);
 
-                        double dist=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+                double dist=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 
-                        String xy_string= parts[1]+","+parts[2];
-
-
-                        return new Tuple2<Double,String>(dist, xy_string);
-                    }
-                });
-
-                //now sort by distance
-                JavaPairRDD<Double, String> sortedDistance = distanceMap.sortByKey(true);
-
-                //remove duplicates
-                JavaPairRDD<Double, String> distinctNeighbors = sortedDistance.distinct();
-
-                //print k neighbors
-                //Integer k= Integer.valueOf(args[2]);
-
-                for(Tuple2<Double, String> line:distinctNeighbors.sortByKey().collect()){
-                    if(knn_.k>0) {
-                        System.out.println("* " + line);
-                        knn_.k--;
-                    }else{
-                        break;
-                    }
-                }
+                String xy_string= parts[1]+","+parts[2];
 
 
-
-
-            }else if(n==2){
-                if(first_run){
-                    decompressFile(localFile);
-                }
-                //call alg2
-            }else if(n==3){
-                return;
+                return new Tuple2<Double,String>(dist, xy_string);
             }
-            System.out.println("\n \n Choose one of the options: \n 1. Spark RDD KNN \n 2. Spark SQL KNN 3. Exit \n \n");
+        });
+
+        //now sort by distance
+        JavaPairRDD<Double, String> sortedDistance = distanceMap.sortByKey(true);
+
+        //remove duplicates
+        JavaPairRDD<Double, String> distinctNeighbors = sortedDistance.distinct();
+
+        //print k neighbors
+        Integer k= Integer.valueOf(args[2]);
+        FileWriter fileWriter = new FileWriter(out_path_rdd);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        System.out.println("-------------------Spark RDD KNN -----------------------");
 
 
+        for(Tuple2<Double, String> line:distinctNeighbors.sortByKey().collect()){
+            if(k>0) {
+                printWriter.println("* " + line);
+                k--;
+            }else{
+                break;
+            }
         }
+        printWriter.close();
 
+
+        /**********************SparkSQL Implementation**********************
+         *
+         */
+        SparkSession session_sql = SparkSession
+                .builder()
+                .appName("Housing Price Feature Correlation")
+                .getOrCreate();
+
+        Dataset<Row> input_csv =session_sql.sql("SELECT * FROM csv.`local_copy.csv`");
+
+
+        System.out.println("\n\nDone. Please check output files\n");
 
     }
 }
+
